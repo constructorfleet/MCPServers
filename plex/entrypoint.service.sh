4a90ca128001bedb91b56ee4c87e1bb3b851ff87
--global <--global>
2025-08-11 18:50:49 -0600

    Script envs

--- plex/entrypoint.service.sh
+++ plex/entrypoint.service.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e  # Crash fast and loud if anything fails
-
+read -r -a args <<< "$SCRIPT_ARGS"
 . .venv/bin/activate
 
-exec uv run "$SERVICE_PACKAGE" \
+exec uv run "$SCRIPT" \
     -l "$LOG_LEVEL" \
     "${args[@]}"
